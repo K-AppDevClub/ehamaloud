3c8047069f383b3aeb7efb2525f407f51b27fe56
sotaro maehara <s.maehara81@icloud.com>
2018-05-19 02:16:56 +0900

    refact

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -35,15 +35,11 @@ export default {
 
   data (){
     return {
-      canvasContext: null,
-      audioAnalyser: null,
-      preSpectrums: [],
-      audioContext:  new AudioContext(),
-      bufferSize:  1024,
-      audioData:  [],
-      recordingFlg: false,
-      t: 0,
-      score: 0,
+      canvasContext: null, audioAnalyser: null,
+      preSpectrums: [], audioData:  [],
+      audioContext: new AudioContext(),
+      bufferSize: 1024, recordingFlg: false,
+      t: 0, score: 0,
       size: {
         width: 500,
         height: 500,
@@ -53,15 +49,10 @@ export default {
 
   methods: {
     startRecording() {
-        this.recordingFlg = true;
-        this.t = 0;
-        this.score = 0;
-        this.canvasContext = this.$refs.scope.getContext('2d');
-        navigator.getUserMedia({audio: true}, this.record, 
-        function(e) {
-            console.log(e);
-        });
-        
+      this.canvasContext = this.$refs.scope.getContext('2d');
+      this.recordingFlg = true;
+      this.t = 0, this.score = 0;
+      navigator.getUserMedia({audio: true}, this.record, (e)=>{ console.log(e) });
     },
 
     record(stream){
@@ -80,61 +71,60 @@ export default {
     },
 
     endRecording(){
-        this.recordingFlg = false;
+      this.recordingFlg = false;
     },
 
     analyseVoice() {
-        var fsDivN = this.audioContext.sampleRate / this.audioAnalyser.fftSize;
-        var spectrums = new Uint8Array(this.audioAnalyser.frequencyBinCount);
-        this.audioAnalyser.getByteFrequencyData(spectrums);
-        this.canvasContext.clearRect(0, 0, this.size.width, this.size.height);
-        this.canvasContext.beginPath();
-        
-        for (var i = 0, len = spectrums.length; i < len; i++) {
-            var x = (i / len) * this.size.width, y = (1 - (spectrums[i] / 255)) * this.size.height;
-            if (i === 0) {
-                this.canvasContext.moveTo(x, y);
-            } else {
-                this.canvasContext.lineTo(x, y);
-            }
-            var f = Math.floor(i * fsDivN);  // index -> frequency;
-            //500 Hz単位にy軸の線とラベル出力
-            if ((f % 500) === 0) {
-                var text = (f < 1000) ? (f + ' Hz') : ((f / 1000) + ' kHz');
-                //Draw grid (X)
-                this.canvasContext.fillRect(x, 0, 1, this.size.height);
-                //Draw text (X)
-                this.canvasContext.fillText(text, x, this.size.height);
-            }
+      var fsDivN = this.audioContext.sampleRate / this.audioAnalyser.fftSize;
+      var spectrums = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+      this.audioAnalyser.getByteFrequencyData(spectrums);
+      this.canvasContext.clearRect(0, 0, this.size.width, this.size.height);
+      this.canvasContext.beginPath();
+      this.drawSpectrums(spectrums, fsDivN)
+    },
 
-            //scoreを計算
-            this.score += Math.pow(spectrums[i] - (this.preSpectrums[i] || 0), 2) / 100
+    drawSpectrums(spectrums, fsDivN){
+      for (var i = 0, len = spectrums.length; i < len; i++) {
+        var x = (i / len) * this.size.width, y = (1 - (spectrums[i] / 255)) * this.size.height;
+        if (i === 0) {
+          this.canvasContext.moveTo(x, y);
+        } else {
+          this.canvasContext.lineTo(x, y);
         }
-        this.preSpectrums = spectrums
-        this.canvasContext.stroke();
+        var f = Math.floor(i * fsDivN);  // index -> frequency;
+        //500 Hz単位にy軸の線とラベル出力
+        if ((f % 500) === 0) {
+          var text = (f < 1000) ? (f + ' Hz') : ((f / 1000) + ' kHz');
+          this.canvasContext.fillRect(x, 0, 1, this.size.height);
+          this.canvasContext.fillText(text, x, this.size.height);
+        }
 
-        // x軸の線とラベル出力
-        var textYs = ['1.00', '0.50', '0.00'];
-        for (var i = 0, len = textYs.length; i < len; i++) {
-            var text = textYs[i];
-            var gy   = (1 - parseFloat(text)) * this.size.height;
-            //Draw grid (Y)
-            this.canvasContext.fillRect(0, gy, this.size.width, 1);
-            // Draw text (Y)
-            this.canvasContext.fillText(text, 0, gy);
-        }
+        //scoreを計算
+        this.score += Math.pow(spectrums[i] - (this.preSpectrums[i] || 0), 2) / 100
+      }
+      this.preSpectrums = spectrums
+      this.canvasContext.stroke();
+
+      // x軸の線とラベル出力
+      var textYs = ['1.00', '0.50', '0.00'];
+      for (var i = 0, len = textYs.length; i < len; i++) {
+        var text = textYs[i];
+        var gy = (1 - parseFloat(text)) * this.size.height;
+        this.canvasContext.fillRect(0, gy, this.size.width, 1);
+        this.canvasContext.fillText(text, 0, gy);
+      }
     },
 
     onAudioProcess(e) {
       if (!this.recordingFlg) return;
-        // 音声のバッファを作成
-        var input = e.inputBuffer.getChannelData(0);
-        var bufferData = new Float32Array(this.bufferSize);
-        for (var i = 0; i < this.bufferSize; i++) bufferData[i] = input[i];
-      
-        // 波形を解析
-        this.analyseVoice();
-        if(this.t++ > 100) this.recordingFlg=false
+      // 音声のバッファを作成
+      var input = e.inputBuffer.getChannelData(0);
+      var bufferData = new Float32Array(this.bufferSize);
+      for (var i = 0; i < this.bufferSize; i++) bufferData[i] = input[i];
+    
+      // 波形を解析
+      this.analyseVoice();
+      if(this.t++ > 100) this.recordingFlg=false
     }
   },
 };
