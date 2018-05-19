076c0259974e3906039544d8219660fcbdb1c2c3
RYUJI <sc113064@ibe.kagoshima-u.ac.jp>
2018-05-19 18:25:46 +0900

    twitter投稿コンポーネント作成

---
+++ src/components/twitter/Twitter.vue
@@ -0,0 +1,33 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  
+</style>
+
+<template>
+	<!-- <v-ons-button @click="postTwitter" modifier="outline" style="margin: 6px 0">{{ msg }}</v-ons-button> -->
+	<a id="link" href="" target="_blank">ツイートする</a>
+	
+</template>
+
+<script>
+export default {
+  name: 'twitter',
+  props: {
+    msg: {
+      default: 'メッセージ',
+		},
+		score: {
+			default: '00',
+		},
+	},
+	mounted() {
+		var target = document.getElementById("link");
+		target.href = `https://twitter.com/intent/tweet?original_referer='元ページのURL'&text=ehama-loudのスコア : ${this.score}点!!&tw_p=tweetbutton&url=https://ja.onsen.io/`;
+	},
+  methods: {
+    // postTwitter() {
+		// 	console.log(this.msg);
+		// },
+  },
+};
+</script>
