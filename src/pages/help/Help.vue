a12090b3f39155bb4394aa1090cab37b8ccbe2a3
Eiji <morinokumakamesan@gmail.com>
2018-06-09 17:01:50 +0900

    MainMenuとHelpの追加とrouterの設定

---
+++ src/pages/help/Help.vue
@@ -0,0 +1,31 @@
+<style lang='scss' scoped>
+.ranklist{
+  width:100%
+}
+.retry{
+  margin: auto;
+}
+.flex-container {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+.action-btn {
+  margin: 0 10px;
+}
+</style>
+
+<template>
+  <ons-page>
+    <v-ons-button  @click="$router.push({ name: 'main-menu'});">戻る</v-ons-button>
+  </ons-page>
+</template>
+
+<script>
+
+export default {
+  name: 'main-menu',
+  components: {
+  },
+};
+</script>
