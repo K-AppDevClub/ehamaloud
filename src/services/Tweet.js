5c3c320baa20bb413a5deb833c02bbe33c4a3acc
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-06-25 02:41:46 +0900

    twitter url コメントアウト

--- src/services/Tweet.js
+++ src/services/Tweet.js
@@ -1,7 +1,8 @@
 export default {
   methods: {
     postTweet(score) {
-      window.open(`https://twitter.com/intent/tweet?original_referer='元ページのURL'&text=元気な人選手権のスコア : ${score}点!!&tw_p=tweetbutton&url=https://ja.onsen.io/`);
+      //window.open(`https://twitter.com/intent/tweet?original_referer='元ページのURL'&text=元気な人選手権のスコア : ${score}点!!&tw_p=tweetbutton&url=https://ja.onsen.io/`);
+      window.open(`https://twitter.com/intent/tweet?original_referer='元ページのURL'&text=元気な人選手権のスコア : ${score}点!!&tw_p=tweetbutton`);
     },
   },
 };
